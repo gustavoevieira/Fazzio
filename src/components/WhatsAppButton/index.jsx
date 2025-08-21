e858fe15f20ff9909c7066f0ebc926a4682942af
gustavoevieira <gustta.eudes@gmail.com>
2025-08-21 15:03:35 -0300

    update assets

--- src/components/WhatsAppButton/index.jsx
+++ src/components/WhatsAppButton/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import Lottie from 'lottie-react';
-import whatsappAnimation from '../../../public/assets/lottie/whatsapp.json';
 import './WhatsAppButton.css';
+import { useEffect, useState } from "react";
 
 const WhatsAppButton = () => {
+  const [animationData, setAnimationData] = useState(null);
+
+  useEffect(() => {
+    fetch("/assets/lottie/WhatsApp.json")
+      .then((res) => res.json())
+      .then(setAnimationData);
+  }, []);
+
+  if (!animationData) return null;
+
   return (
-    <a 
+    <a
       href="https://api.whatsapp.com/send?phone=5516997935919"
       target="_blank"
       rel="noopener noreferrer"
       className="whatsapp-button"
     >
-      <Lottie 
-        animationData={whatsappAnimation} 
-        loop={true} 
-        style={{ width: 200, height: 200 }} 
-      />
+      <Lottie animationData={animationData} loop style={{ width: 200, height: 200 }} />
     </a>
   );
 };
